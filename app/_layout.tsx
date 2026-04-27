4a0bd0fe75093f09e53b70aeac0d360a2388fa8d
go-daniela.dev <go.daniela.dev@gmail.com>
2026-04-28 01:20:49 +0900

    feat: add signup and login flow

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,46 @@
-import { Stack } from "expo-router";
+import useAuth from "@/hooks/queries/useAuth";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import "react-native-reanimated";
 
 export const unstable_settings = {
   anchor: "(tabs)",
 };
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    spaceMono: require("../assets/fonts/Roboto-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    SplashScreen.preventAutoHideAsync();
+  }, []);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RootNavigator />
+    </QueryClientProvider>
+  );
+}
+
+function RootNavigator() {
+  const { auth } = useAuth();
+  console.log("auth", auth);
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
